9063d145779a2cc944e7514c9f97224aae2d7308
Tomas Vink <tv@eenwereldmetlef.be>
2018-09-13 17:04:54 +0200

    updating react dependancy

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
-  name: "lef:imgupload",
-  version: "1.1.5",
-  summary: "File image upload to S3"
-});
+  name: 'lef:imgupload',
+  version: '1.1.6',
+  summary: 'File image upload to S3'
+})
 
-Package.onUse(function(api) {
-  api.use(["ecmascript", "edgee:slingshot"]);
+Package.onUse(function (api) {
+  api.use(['ecmascript', 'edgee:slingshot'])
   Npm.depends({
-    lodash: "4.17.5",
-    react: "16.3.0",
-    reactstrap: "5.0.0"
-  });
-  api.addFiles("Server.js", "server");
-  api.mainModule("Component.js", "client");
-});
+    lodash: '4.17.5',
+    react: '16.5.0',
+    reactstrap: '5.0.0'
+  })
+  api.addFiles('Server.js', 'server')
+  api.mainModule('Component.js', 'client')
+})
