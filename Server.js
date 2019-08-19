84d94d55e5a2b532a3e56c323ef91a12f49018b1
johanlef <jb@eenwereldmetlef.be>
2019-08-19 15:00:53 +0200

    Fix serverside uploads iteration

--- Server.js
+++ Server.js
@@ -2,15 +2,20 @@ import { Meteor } from 'meteor/meteor'
 import './Settings'
 import { Slingshot } from 'meteor/edgee:slingshot'
 
-Meteor.settings.public.uploads.forEach(({ key, defaultPrefix }) => {
-  const server = Meteor.settings.uploads.find(p => p.key === key) || {}
-  Slingshot.createDirective(key, Slingshot.S3Storage, {
-    bucket: server.S3Bucket || Meteor.settings.S3Bucket,
-    acl: 'public-read',
-    cacheControl: 'max-age=3153600',
-    region: server.S3Region || Meteor.settings.S3Region,
-    authorize: () => true,
-    key: ({ name }) =>
-      key + '/' + (name.indexOf('/') ? name : (defaultPrefix || '') + name)
+const uploads = Meteor.settings.public.uploads
+
+if (uploads) {
+  uploads.forEach(({ key, defaultPrefix }) => {
+    const serverSide = Meteor.settings.uploads || []
+    const server = serverSide.find(p => p.key === key) || {}
+    Slingshot.createDirective(key, Slingshot.S3Storage, {
+      bucket: server.S3Bucket || Meteor.settings.S3Bucket,
+      acl: 'public-read',
+      cacheControl: 'max-age=3153600',
+      region: server.S3Region || Meteor.settings.S3Region,
+      authorize: () => true,
+      key: ({ name }) =>
+        key + '/' + (name.indexOf('/') ? name : (defaultPrefix || '') + name)
+    })
   })
-})
+}
--- Settings.js
+++ Settings.js
@@ -2,14 +2,17 @@ import { Meteor } from 'meteor/meteor'
 import { Slingshot } from 'meteor/edgee:slingshot'
 
 const uploads = Meteor.settings.public.uploads
-uploads.forEach(({ key, maxSize, allowedFileTypes, ...upload }) => {
-  const restrictions = {
-    allowedFileTypes: allowedFileTypes || [
-      'image/png',
-      'image/jpeg',
-      'image/gif'
-    ],
-    maxSize: maxSize || 12582912
-  }
-  Slingshot.fileRestrictions(key, restrictions)
-})
+
+if (uploads) {
+  uploads.forEach(({ key, maxSize, allowedFileTypes, ...upload }) => {
+    const restrictions = {
+      allowedFileTypes: allowedFileTypes || [
+        'image/png',
+        'image/jpeg',
+        'image/gif'
+      ],
+      maxSize: maxSize || 12582912
+    }
+    Slingshot.fileRestrictions(key, restrictions)
+  })
+}
