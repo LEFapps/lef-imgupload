fa75f31d90574a311dc9b3280b46bd07e3286b46
johanlef <jb@eenwereldmetlef.be>
2020-05-27 10:15:26 +0200

    Update @lefapps/uploader

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lef:imgupload',
-  version: '2.0.0',
+  version: '2.0.1',
   summary: 'Upload directly to S3'
 })
 
@@ -8,7 +8,7 @@ Package.onUse(function (api) {
   api.use(['ecmascript', 'edgee:slingshot'])
   Npm.depends({
     lodash: '4.17.15',
-    '@lefapps/uploader': '0.0.2'
+    '@lefapps/uploader': '0.0.5'
   })
   api.addFiles('server.js', 'server')
   api.mainModule('Component.js', 'client')
